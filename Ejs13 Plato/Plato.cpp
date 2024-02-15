3057e51b4b173301a2d438717f09382d2f1b6745
matwate <mateowatata@gmail.com>
2024-02-15 14:17:39 -0500

    Refactored menu options and added new history feature

--- Ejs13 Plato/Plato.cpp
+++ Ejs13 Plato/Plato.cpp
@@ -3,37 +3,44 @@
 using namespace std;
 
 int main() {
-    string opcionesCarbohidrato[3] = {"Pasta", "Arroz", "Papa"};
-    int preciosCarbohidrato[3] = {10200, 8500, 15000};
-
-    string opcionesCarne[4] = {"Pollo", "Res", "Pescado", "Cerdo"};
-    int preciosCarne[4] = {8000, 12000, 15000, 10000};
     
-    string opcionesBebida[5] = {"Agua", "Jugo", "Gaseosa", "Limonada", "Te"};
-    int preciosBebida[5] = {2000, 3000, 4000, 5000, 6000};
-    
-    string opcioneesAcompanamiento[2] = {"Ensalada", "Papas"};
-    int preciosAcompanamiento[2] = {3000, 4000};
-    
-    int carbohidrato, carne, bebida, acompanamiento;
-    int total;
+    string platos[] = {"Pizza", "Hamburguesa", "Tacos"};
+    int preciosBase[] = {21000, 18000, 15000};   
+    string toppingsPizza[] = {"Pepperoni", "Jamon", "Champiñones"}; 
+    int preciosToppingsPizza[] = {2000, 3000, 4000};
+    string toppingsHamburguesa[] = {"Queso", "Lechuga", "Cebolla"};
+    int preciosToppingsHamburguesa[] = {1000, 500, 500};
 
-    cout << "Escoja su carbohidrato: 0: Pasta,10200$ 1: Arroz,8500$ 2: Papa,15000$: ";
-    cin >> carbohidrato;
+    string toppingsTacos[] = {"Cilantro", "Cebolla", "Salsa"};
+    int preciosToppingsTacos[] = {500, 500, 1000};
 
-    cout << "Escoja su carne: 0: Pollo,8000$ 1: Res,12000$ 2: Pescado,15000$ 3: Cerdo,10000$: ";
-    cin >> carne;
+    int opcionPlato;
+    cout << "Seleccione un plato 0:Pizza,21000$ 1:Hamburguesa,18000$ 2:Tacos, 15000$: " << endl;
+    cin >> opcionPlato;
 
-    cout << "Escoja su bebida: 0: Agua,2000$ 1: Jugo,3000$ 2: Gaseosa,4000$ 3: Limonada,5000$ 4: Te,6000$: ";
-    cin >> bebida;
+    cout << "Usted selecciono: " << platos[opcionPlato] << endl;
 
-    cout << "Escoja su acompanamiento: 0: Ensalada,3000$ 1: Papas,4000$: ";
-    cin >> acompanamiento;
-
-    total = preciosCarbohidrato[carbohidrato] + preciosCarne[carne] + preciosBebida[bebida] + preciosAcompanamiento[acompanamiento];
-    cout << "El plato escogido es: " << opcionesCarbohidrato[carbohidrato] << " con " << opcionesCarne[carne] << " con " << opcionesBebida[bebida] << " y " << opcioneesAcompanamiento[acompanamiento] << endl;
-    cout << "El total a pagar es: " << total << endl;
-
+    if(opcionPlato == 0){
+        cout << "Seleccione un topping 0:Pepperoni,2000$ 1:Jamon,3000$ 2:Champiñones,4000$: " << endl;
+        int opcionTopping;
+        cin >> opcionTopping;
+        cout << "Usted selecciono: " << toppingsPizza[opcionTopping] << endl;
+        cout << "El precio total es: " << preciosBase[opcionPlato] + preciosToppingsPizza[opcionTopping] << endl;
+    }else if(opcionPlato == 1){
+        cout << "Seleccione un topping 0:Queso,1000$ 1:Lechuga,500$ 2:Cebolla,500$: " << endl;
+        int opcionTopping;
+        cin >> opcionTopping;
+        cout << "Usted selecciono: " << toppingsHamburguesa[opcionTopping] << endl;
+        cout << "El precio total es: " << preciosBase[opcionPlato] + preciosToppingsHamburguesa[opcionTopping] << endl;
+    }else if(opcionPlato == 2){
+        cout << "Seleccione un topping 0:Cilantro,500$ 1:Cebolla,500$ 2:Salsa,1000$: " << endl;
+        int opcionTopping;
+        cin >> opcionTopping;
+        cout << "Usted selecciono: " << toppingsTacos[opcionTopping] << endl;
+        cout << "El precio total es: " << preciosBase[opcionPlato] + preciosToppingsTacos[opcionTopping] << endl;
+    }else{
+        cout << "Opcion no valida" << endl;
+    }
 
     return 0;
 }
